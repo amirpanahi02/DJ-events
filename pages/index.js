52272c902e85fd60fbcc29e66840910e26d5f0a2
Amirhossein Panahi <76567918+amirpanahi02@users.noreply.github.com>
2022-04-16 17:10:59 +0430

    displaying events

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,32 @@
 import Layout from "./../components/Layout";
 import { API_URL } from "./../config/index";
+import EventItem from "../components/EventItem";
+import Link from "next/link";
 
-export default function Home({ events }) {
-  console.log(events);
+export default function HomePage({ events }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show :(</h3>}
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
   );
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const res = await fetch(`${API_URL}/api/events`);
   const events = await res.json();
   return {
     props: {
-      events,
+      events: events.slice(0, 3),
     },
+    revalidate: 1,
   };
 }
