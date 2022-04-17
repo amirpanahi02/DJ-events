3e57df5bbf92f16cab512d05ec6c6836a7ca1bc4
Amirhossein Panahi <76567918+amirpanahi02@users.noreply.github.com>
2022-04-17 16:06:23 +0430

    fetch single events

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,10 +1,44 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import { API_URL } from "../../config";
 
-export default function EventPage() {
+export default function EventPage({ evt }) {
   return (
     <Layout>
-      <div>my event</div>
+      <h1>{evt.name}</h1>
     </Layout>
   );
+}
+
+// export async function getServerSideProps({ query: { slug } }) {
+//   const res = await fetch(`${API_URL}/api/events/${slug}`);
+//   const events = await res.json();
+//   return {
+//     props: {
+//       evt: events[0],
+//     },
+//   };
+// }
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  const paths = events.map((evt) => ({
+    params: { slug: evt.slug },
+  }));
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+  return {
+    props: {
+      evt: events[0],
+    },
+  };
 }
