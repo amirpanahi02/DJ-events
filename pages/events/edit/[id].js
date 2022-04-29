8f1c7643f044e79f0b59b199faeeee4f2809038e
Amirhossein Panahi <76567918+amirpanahi02@users.noreply.github.com>
2022-04-29 21:25:11 +0430

    fix git

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -11,10 +11,7 @@ import Layout from "../../../components/Layout";
 import { API_URL } from "../../../config/index";
 import Image from "next/image";
 import Modal from "./../../../components/Modal";
-<<<<<<< HEAD
 import ImageUpload from "../../../components/ImageUpload";
-=======
->>>>>>> 3892a021b4ed0d761dd248254c14469a1ce890e7
 
 export default function EditEventPage({ evt }) {
   const [values, setValues] = useState({
@@ -169,13 +166,10 @@ export default function EditEventPage({ evt }) {
           <FaImage /> Set Image
         </button>
       </div>
-<<<<<<< HEAD
       <Modal show={showModal} onClose={() => setShowModal(false)}>
         <ImageUpload evtId={evt.id} imageUploaded={imageUploaded} />
       </Modal>
-=======
       <Modal show={showModal} onClose={() => setShowModal(false)}></Modal>
->>>>>>> 3892a021b4ed0d761dd248254c14469a1ce890e7
     </Layout>
   );
 }
