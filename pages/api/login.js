e0382be958d9a28999cdc065f338817613fd4966
Amirhossein Panahi <76567918+amirpanahi02@users.noreply.github.com>
2022-05-04 19:14:32 +0430

    saving jwt as a http cookie

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,4 +1,5 @@
 import { API_URL } from "./../../config/index";
+import cookie from "cookie";
 
 const login = async (req, res) => {
   if (req.method === "POST") {
@@ -12,11 +13,23 @@ const login = async (req, res) => {
     });
 
     const data = await strapiRes.json();
-    strapiRes.ok
-      ? res.status(200).json(data)
-      : res
-          .status(data.statusCode)
-          .json({ message: data.message[0].messages[0].message, a: 6 });
+    if (strapiRes.ok) {
+      res.setHeader(
+        "Set-Cookie",
+        cookie.serialize("token", data.jwt, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV !== "development",
+          maxAge: 60 * 60 * 24 * 7, // 1 week
+          sameSite: "strict",
+          path: "/",
+        })
+      );
+      res.status(200).json({ user: data.user });
+    } else {
+      res.status(data.statusCode).json({
+        message: data.message[0].messages[0].message,
+      });
+    }
   } else {
     res.setHeader("allow"["POST"]);
     res.status(405).json({ message: `Method ${req.method} not allowed` });
